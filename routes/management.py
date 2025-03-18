53f28081260878786a51281fbafffc291f3255db
Jh0nn1er <jhonnier-gomez@finanzauto.com.co>
2025-03-17 23:02:47 -0500

    fix(href-menu): agregar href for menus

--- routes/management.py
+++ routes/management.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template, request, jsonify
 management_bp = Blueprint('gestion', __name__)
 
 # ➡ **Rutas del front para balance financiero**
-@management_bp.route('/gestion', methods=['GET'])
+@management_bp.route('/management', methods=['GET'])
 def balance_page():
     return render_template('management.html')
--- routes/profiability.py
+++ routes/profiability.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template, request, jsonify
 profiability_bp = Blueprint('porfiability', __name__)
 
 # ➡ **Rutas del front para balance financiero**
-@profiability_bp.route('/rentabilidad', methods=['GET'])
+@profiability_bp.route('/profiability', methods=['GET'])
 def balance_page():
     return render_template('profiability.html')
