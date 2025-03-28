796134eb32c7099a553324429112c3d9f3491ab6
Jh0nn1er <jhonnier-gomez@finanzauto.com.co>
2025-03-27 20:02:12 -0500

    fix(Route):update routes users

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -4,29 +4,29 @@ from controllers.user_controller import register_user, login_user, reset_passwor
 user_bp = Blueprint("user_bp", __name__)
 
 # Ruta para mostrar el formulario de login
-@user_bp.route("/login", methods=["GET"])
+@user_bp.route("/users/login", methods=["GET"])
 def login_page():
     return render_template("login.html")
 
 # Ruta para manejar el login
-@user_bp.route("/login", methods=["POST"])
+@user_bp.route("/users/login", methods=["POST"])
 def login():
     return login_user()
 
 # Ruta para mostrar el formulario de registro
-@user_bp.route("/register", methods=["GET"])
+@user_bp.route("/users/register", methods=["GET"])
 def register_page():
     return render_template("register.html")
 
 # Ruta para manejar el registro de usuarios
-@user_bp.route("/register", methods=["POST"])
+@user_bp.route("/users/register", methods=["POST"])
 def register():
     return register_user()
 
-@user_bp.route("/generate_password", methods=["POST"])
+@user_bp.route("/users/generate_password", methods=["POST"])
 def reset():
     return reset_password()
 
-@user_bp.route("/generate_password", methods=["GET"])
+@user_bp.route("/users/generate_password", methods=["GET"])
 def generated_password():
     return render_template("generate_password.html")
